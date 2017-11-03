5e61fce64c40683e82c34615e46c708e4232fae2
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-03 17:35:16 -0600

    streamline all the control branches in playTicTacToe()

--- tictactoeserver.go
+++ tictactoeserver.go
@@ -28,19 +28,22 @@ func main() {
 }
 
 func playTicTacToe(conn net.Conn) (int, error) {
+	const CLIENTWON = "client won"
+	const SERVERWON = "server won"
 	const SERVERSYMBOL = 'O'
 	const CLIENTSYMBOL = 'X'
+
 	squares := []int{0, 1, 2, 4, 5, 6, 8, 9, 10}
+
 	var rBoard string
 	var sBoard string = tictactoe.GetEmptyBoard()
 
 	var n int
 	var err error
 
-	var moved bool
+	var clientWon, serverWon bool
 
 	for {
-		moved = false
 		bytesFromClient := make([]byte, 11)
 		n, err = conn.Read(bytesFromClient)
 		if err != nil	{
@@ -58,41 +61,23 @@ func playTicTacToe(conn net.Conn) (int, error) {
 			return n, fmt.Errorf("playTicTacToe() client made %d moves", movCnt)
 		}
 
-		// if tictactoe.HasWon(rBoard, CLIENTSYMBOL)	{
-		// 	fmt.Println("Client won!")
-		// 	sBoard = "END"
-		// } else if condition {
-		//
-		// }
-
 		if tictactoe.HasWon(rBoard, CLIENTSYMBOL)	{
-			// check is the opponent has won
-			fmt.Println("Client won!")
-			n, err = conn.Write([]byte("END"))
-			if err != nil {
-				return n, fmt.Errorf("playTicTacToe error while writing %v", sBoard)
-			}
-			break
-		}
-
-		if win, ptrn := tictactoe.CanWinNext(rBoard, SERVERSYMBOL); win == true	{
-			// fmt.Println("Server can win!")
+			// check if the opponent has won
+			sBoard = CLIENTWON
+			clientWon = true
+		} else if win, ptrn := tictactoe.CanWinNext(rBoard, SERVERSYMBOL); win {
+			// check if we can win in one move; make that move
 			sBoard, _ = tictactoe.MakeWinMove(rBoard, ptrn, SERVERSYMBOL)
-			moved = true
-		} else if win, ptrn := tictactoe.CanWinNext(rBoard, CLIENTSYMBOL); win == true	{
-			// fmt.Println("Client can win! ", ptrn)
+			serverWon = true
+		} else if win, ptrn := tictactoe.CanWinNext(rBoard, CLIENTSYMBOL); win {
+			// check if opponent can win in one move; block that move
 			sBoard, _ = tictactoe.BlockWinMove(rBoard, ptrn, SERVERSYMBOL)
-			moved = true
-		}
-
-		if !moved	{
+		} else {
+			// make a random move
 			sBoard, err = tictactoe.MakeRandomMove(rBoard, squares, SERVERSYMBOL)
 			if err != nil {
-				n, err = conn.Write([]byte("END"))
-				if err != nil {
-					return n, fmt.Errorf("playTicTacToe error while writing %v", sBoard)
-				}
-				break
+				// error indicates there are no more free positions; send END signal
+				sBoard = "END"
 			}
 		}
 
@@ -101,6 +86,10 @@ func playTicTacToe(conn net.Conn) (int, error) {
 			return n, fmt.Errorf("playTicTacToe error while writing %v", sBoard)
 		}
 		fmt.Printf(" S: %q\n", sBoard)
+
+		if sBoard == "END" || serverWon || clientWon	{
+			break
+		}
 	}
 
 	return 0, nil
