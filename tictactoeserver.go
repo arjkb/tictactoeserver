d2722b7c16d35c63e44f94027e68b4d63081065e
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-11-02 22:41:22 -0600

    remove obsolete code

--- tictactoeserver.go
+++ tictactoeserver.go
@@ -25,8 +25,6 @@ func main() {
 	if err != nil	{
 		log.Fatalf(" main() n=%v: %v", n, err)
 	}
-
-	// handleConnection(conn)
 }
 
 func playTicTacToe(conn net.Conn) (int, error){
@@ -59,35 +57,4 @@ func playTicTacToe(conn net.Conn) (int, error){
 		}
 		fmt.Printf(" SENT: %q\n", movedBoard)
 	}
-}
-
-func handleConnection(conn net.Conn) {
-	msg, n, err := receiveMsg(conn)
-	if err != nil {
-		fmt.Println(" handleConnection: ", err)
-	}
-	fmt.Printf("%v bytes read\n", n)
-	fmt.Println("Client says:")
-	fmt.Println(msg)
-
-	n, err = sendMsg(conn, "END")
-	conn.Close()
-}
-
-func receiveMsg(conn net.Conn) (msg string, n int, err error) {
-	var msg_bytes = make([]byte, 100)
-	n, err = conn.Read(msg_bytes)
-	if err != nil {
-		return msg, n, fmt.Errorf(" receiveMsg: %v", err)
-	}
-
-	return string(msg_bytes), n, err
-}
-
-func sendMsg(conn net.Conn, msg string) (n int, err error) {
-	n, err = conn.Write([]byte(msg))
-	if err != nil {
-		return n, fmt.Errorf(" error while writing: ", err)
-	}
-	return n, nil
 }
